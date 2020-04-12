663688b08087c6f6f6402f99bdbe7f85291f2853
PeterQu <peterqu007@gmail.com>
2020-04-12 11:12:47 -0700

    contect js for stockhouse

--- chrome_extension/stockHouseBullboard.js
+++ chrome_extension/stockHouseBullboard.js
@@ -1,1 +1,25 @@
-console.log("Hello Stockhouse");
+//p$lt$zoneContent$SubContent$p$lt$zoneLeft$Stockhouse_CompanyBullboard$viewerForum$threadsElem$drpViewModeSelector
+//p$lt$zoneContent$SubContent$p$lt$zoneLeft$Stockhouse_CompanyBullboard$viewerForum$threadsElem$drpViewModeSelector
+
+class stockhouse {
+  constructor() {
+    this.name = "test";
+    this.htmlSelect = $(
+      '[name="p$lt$zoneContent$SubContent$p$lt$zoneLeft$Stockhouse_CompanyBullboard$viewerForum$threadsElem$drpViewModeSelector"]'
+    );
+  }
+  getName() {
+    console.log("hello ", this.name);
+  }
+  getSelectItem() {
+    let htmlOption = this.htmlSelect.children("option:selected");
+    console.log(htmlOption.val());
+  }
+  setSelectItem() {
+    let htmlOption = this.htmlSelect.children("option:selected");
+    htmlOption.val("Threaded");
+  }
+}
+
+const sh = new stockhouse();
+sh.setSelectItem();
